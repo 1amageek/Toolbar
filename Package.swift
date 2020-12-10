66641ae33d80e4aa3f90d1510400051c68389e8b
1amageek <tmy0x3@icloud.com>
2020-12-10 20:02:45 +0900

    Fix

--- Package.swift
+++ Package.swift
@@ -11,10 +11,6 @@ let package = Package(
             name: "Toolbar",
             targets: ["Toolbar"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         .target(
             name: "Toolbar",
